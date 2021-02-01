d51126173410cc6444a75a973c84e3f03284cb02
BIDISHA-CHANDA <67823051+BIDISHA-CHANDA@users.noreply.github.com>
2021-02-02 01:51:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,36 +2,42 @@
 
 
 
-from flask import Flask,request, url_for, redirect, render_template
-import pickle
 import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
 
 app = Flask(__name__)
-
-model=pickle.load(open('co2_final.pkl','rb'))
-
+model = pickle.load(open('co2_final.pkl', 'rb'))
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def home():
+  return render_template('index.html')
 
-
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=model.predict_proba(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
+  input_features = [int(x) for x in request.form.values()]
+  features_value = [np.array(input_features)]
 
-    if output>str(0.5):
-        return render_template('index.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
-    else:
-        return render_template('index.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
+  features_name = ['Engine_Size(L)', 'Cylinders',
+        'Fuel_Consumption_City_(L/100 km)',
+       'Fuel_Consumption_Hwy_(L/100 km)', 'Fuel_Consumption_Comb_(L/100 km)',
+       'Fuel_Consumption_Comb_(mpg)']
+
+  df = pd.DataFrame(features_value, columns=features_name)
+  output = model.predict(df)
+
+  if output >= 300:
+      res_val = "Breast cancer"
+  else:
+      res_val = "no Breast cancer"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+  return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
+  
+
+if __name__ == "__main__":
+  app.run()
+
 
     
