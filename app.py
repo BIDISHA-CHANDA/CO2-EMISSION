18370e43def49016a3aa528d17a6ff7927f6232e
BIDISHA-CHANDA <67823051+BIDISHA-CHANDA@users.noreply.github.com>
2021-02-02 01:29:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,57 @@
-import numpy as np
-import pandas as pd
-from flask import Flask, request, render_template
+
+Skip to content
+Pull requests
+Issues
+Marketplace
+Explore
+@BIDISHA-CHANDA
+Learn Git and GitHub without any code!
+
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+nachi-hebbar /
+Forest-Fire-Prediction-Website
+
+1
+40
+
+    46
+
+Code
+Issues
+Pull requests 2
+Actions
+Projects
+Wiki
+Security
+
+    Insights
+
+Forest-Fire-Prediction-Website/app.py /
+
+
+
+from flask import Flask,request, url_for, redirect, render_template
 import pickle
+import numpy as np
 
 app = Flask(__name__)
-model = pickle.load(open('co2_final.pkl', 'rb'))
+
+model=pickle.load(open('model.pkl','rb'))
+
 
 @app.route('/')
-def home():
-  return render_template('index.html')
+def hello_world():
+    return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict',methods=['POST','GET'])
 def predict():
-  input_features = [int(x) for x in request.form.values()]
-  features_value = [np.array(input_features)]
-
-  features_name = ['Engine_Size(L)', 'Cylinders',
-        'Fuel_Consumption_City_(L/100 km)',
-       'Fuel_Consumption_Hwy_(L/100 km)', 'Fuel_Consumption_Comb_(L/100 km)',
-       'Fuel_Consumption_Comb_(mpg)']
-
-  df = pd.DataFrame(features_value, columns=features_name)
-  prediction=model.predict_proba(df)
-  output='{0:.{1}f}'.format(prediction[0][1], 2)
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction=model.predict_proba(final)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
 
     if output>str(0.5):
         return render_template('index.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
@@ -30,6 +59,7 @@ def predict():
         return render_template('index.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
-if __name__ == "__main__":
-  app.run()
+    
