ae7d8300eda43ae2d33af3f867dbf250500950b9
BIDISHA-CHANDA <67823051+BIDISHA-CHANDA@users.noreply.github.com>
2021-02-02 01:17:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ def predict():
        'Fuel_Consumption_Comb_(mpg)']
 
   df = pd.DataFrame(features_value, columns=features_name)
-  output = model.predict(df)
+  prediction=model.predict_proba(df)
+  output='{0:.{1}f}'.format(prediction[0][1], 2)
 
-  if output == 4:
-      res_val = "Breast cancer"
-  else:
-      res_val = "no Breast cancer"
+    if output>str(0.5):
+        return render_template('index.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
+    else:
+        return render_template('index.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
 
 
-  return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
 
 if __name__ == "__main__":
   app.run()
