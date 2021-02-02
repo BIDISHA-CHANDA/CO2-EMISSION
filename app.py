db6aa60bca810eb6107b1b42483c1946f430770c
BIDISHA-CHANDA <67823051+BIDISHA-CHANDA@users.noreply.github.com>
2021-02-02 11:50:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-  input_features = [int(x) for x in request.form.values()]
+  input_features = [float(x) for x in request.form.values()]
   features_value = [np.array(input_features)]
 
   features_name = ['Engine Size(L)', 'Cylinders',
